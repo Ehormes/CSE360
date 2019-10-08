c639df7c97192876bea6484d6f177fa3ecac8976
Ghost <Ghost@DESKTOP-F61DDT1.3521.dhcp.asu.edu>
2019-10-08 16:03:08 -0700

    First Commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,58 @@
+package cse360assign2;
+
+/**
+ * @author Edmone Hermiz
+ * ID: 1207447679
+ * Assignment 2
+ * 
+ * A simple calculator application
+ * 
+ */
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () {
+		
+		total = 0;  // not needed - included for clarity
+		
+		}
+	
+	/**
+	 * @return total
+	 */
+	public int getTotal () {
+		
+		return 0;
+		
+		}
+	
+	/**
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString () {
+		
+		return "";
+		
+		}
+	
+	public void clear() {
+		
+	}
+	
+}
